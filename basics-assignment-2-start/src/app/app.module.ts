2ca3ecd98c096c6478d91b34a0e18ff320e9daa1
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-05-07 00:02:29 -0600

    add second assignment

--- basics-assignment-2-start/src/app/app.module.ts
+++ basics-assignment-2-start/src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { warningAlertComponent } from './warningAlert/warningAlert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    warningAlertComponent,
-    SuccessAlertComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
