f7bd61a9b14fe1b0884c66ecce9a84aabc10ff78
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-05-08 14:15:22 -0600

    assignment3 toggle secret display

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displaySecret = false;
+  toggleDisplay() {
+    this.displaySecret=!this.displaySecret;
+  }
 }
